ec0728d603a2c6b24360d3767844a3099a463bbc
LeeSeungHui <hithere1012@naver.com>
2021-09-12 00:11:04 +0900

    pong

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/ping", methods=['GET'])
 def ping():
-    return "pong"
+    return jsonify({"result":"pong"})
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=3333)
